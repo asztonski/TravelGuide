f453785210319bb48db4ec348c355a32800200b3
Dominik Supiński <asztonski@gmail.com>
2020-07-24 19:31:11 +0100

    Add new section, improve menu button, add down arrow button

--- Nowa strona Jamajka/scripts/script.js
+++ Nowa strona Jamajka/scripts/script.js
@@ -6,3 +6,17 @@ const menuButtonClick = () => {
 
 const menuButton = document.querySelector('.menuButton');
 menuButton.addEventListener('click', menuButtonClick);
+
+// const body = document.querySelector("body").addEventListener('click', function() {
+//     if (menuButton.classList.contains("active")) {
+//         menuButton.classList.remove("active")
+//     };
+// })
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const header = document.querySelector("header");
+    if (scrollPosition > header.clientHeight - 20 ) {
+        menuButton.classList.add("black")
+    } else menuButton.classList.remove("black");
+});
