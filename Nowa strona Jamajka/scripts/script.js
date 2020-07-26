a9689f3c7112f3ad0253461a75a580878b690cde
Dominik Supiński <asztonski@gmail.com>
2020-07-26 23:08:06 +0100

    Improve side content changing funtion

--- Nowa strona Jamajka/scripts/script.js
+++ Nowa strona Jamajka/scripts/script.js
@@ -26,14 +26,44 @@ menuButton.forEach(item => {
 
 const mainContent = document.querySelector('.main');
 const SLUContent = document.querySelector('.StLucia');
+const JAMContent = document.querySelector('.Jamaica');
 const Flag = document.querySelectorAll('.loadingFlag img');
-// const FlagBox = document.querySelector('loadingFlag');
+
 const BtnStLucia = document.querySelector('.SLU').addEventListener('click', function() {
     if (!mainContent.classList.contains("disactive")) {
         mainContent.classList.add("disactive");
         SLUContent.classList.add("active");
         Flag[0].classList.add("active");
-        // FlagBox.classList.add("active");
+        setTimeout(function () {
+            mainContent.classList.add('none');
+            SLUContent.classList.add('block');
+          }, 1000);
+    }
+})
+
+
+const BtnJamaica = document.querySelector('.JAM').addEventListener('click', function() {
+    if (!mainContent.classList.contains("disactive")) {
+        mainContent.classList.add("disactive");
+        JAMContent.classList.add("active");
+        Flag[1].classList.add("active");
+        setTimeout(function () {
+            mainContent.classList.add('none');
+            JAMContent.classList.add('block');
+          }, 1000);
+    }
+})
+
+const BtnMain = document.querySelector('.BtnMain').addEventListener('click', function() {
+    if (SLUContent.classList.contains("active")) {
+        SLUContent.classList.remove("active");
+        mainContent.classList.remove("disactive");
+        Flag[0].classList.remove("active");
+        Flag[0].classList.add("default");
+        setTimeout(function () {
+            mainContent.classList.remove('none');
+            SLUContent.classList.remove('block');
+          }, 1000);
     }
 })
 
